ecb74f8b3abc30e935d00ffbfa16dff0a744cc93
kazel1990 <kazel1990@gmail.com>
2017-10-15 16:23:37 +0900

    add convolution

--- generator.cpp
+++ generator.cpp
@@ -18,6 +18,52 @@ void print(const char * str)
     puts(str);
 }
 
+//TODO: handle hyperparams outside
+void convolution(std::string name, std::string bot, std::string top,
+        int output, int pad, int kernel, int stride)
+{
+    print("layer {");
+    sprintf(buf,"name: \"%s\"",name.c_str());
+    print("type: \"Convolution\"");
+    sprintf(buf,"bottom: \"%s\"",bot.c_str());
+    print(buf);
+    sprintf(buf,"top: \"%s\"",top.c_str());
+    print(buf);
+
+    print("param {");
+    print("lr_mult: 1");
+    print("decay_mult: 1");
+    print("}");
+    print("param {");
+    print("lr_mult: 2");
+    print("decay_mult: 0");
+    print("}");
+
+    print("convolution_param {");
+    sprintf(buf,"num_output: %d",output);
+    print(buf);
+    sprintf(buf,"pad: %d",pad);
+    print(buf);
+    sprintf(buf,"kernel_size: %d",kernel);
+    print(buf);
+    sprintf(buf,"stride: %d",stride);
+    print(buf);
+
+    print("weight_filler {");
+    print("type: \"xavier\"");
+    print("std: 0.01");
+    print("}");
+
+    print("bias_filler {");
+    print("type: \"constant\"");
+    print("value: 0.2");
+    print("}");
+
+    print("}");
+
+    print("}");
+}
+
 void create_data()
 {
     for(int i=0;i<=1;i++)
@@ -56,8 +102,14 @@ void create_data()
     }
 }
 
+void create_stem()
+{
+    convolution("stem_conv1_3x3", "data", "stem_conv1_3x3",32,0,3,2);
+}
+
 int main()
 {
     freopen("train_val.prototxt","w",stdout);
     create_data();
+    create_stem();
 }
